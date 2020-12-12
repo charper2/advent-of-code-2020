431deb6344df067682dafd1494deacdc6d3e67d8
Caitlin Harper <caitlin.harper@uqconnect.edu.au>
2020-12-12 10:19:09 +1000

    Day Eleven

--- src/main/java/charper/advent/DayEleven.java
+++ src/main/java/charper/advent/DayEleven.java
@@ -11,37 +11,51 @@ public class DayEleven {
 
     public DayEleven() {
         run();
+        run2();
     }
 
     public void run() {
         Map<Integer, char[]> chairMap = getCharMap(FILE_PATH);
         while(true) {
-            Map<Integer, char[]> newChairMap = occupySeatsRound(chairMap);
+            Map<Integer, char[]> newChairMap = occupySeatsPart1(chairMap);
             if (areEqualWithArrayValue(chairMap, newChairMap)) {
                 break;
             }
             chairMap = newChairMap;
         }
-        System.out.println(numOccupiedSeats(chairMap));
+        System.out.println("Part 1: " + numOccupiedSeats(chairMap));
     }
 
-    public Map<Integer, char[]> occupySeatsRound(final Map<Integer, char[]> chairMap) {
+    public void run2() {
+        Map<Integer, char[]> chairMap = getCharMap(FILE_PATH);
+        while(true) {
+            Map<Integer, char[]> newChairMap = occupySeatsPart2(chairMap);
+            if (areEqualWithArrayValue(chairMap, newChairMap)) {
+                break;
+            }
+            chairMap = newChairMap;
+        }
+        System.out.println("Part 2: " + numOccupiedSeats(chairMap));
+    }
+
+    private Map<Integer, char[]> occupySeatsPart1(final Map<Integer, char[]> chairMap) {
         Map<Integer, char[]> newChairMap = deepClone(chairMap);
         int maxX = chairMap.get(0).length;
-        System.out.println(chairMap.get(0));
         int maxY = chairMap.keySet().size();
         for (int i = 0; i < maxX ; i++) {
             for (int j = 0; j < maxY; j++) {
                 char c = chairMap.get(j)[i];
                 if (isEmpty(c)) {
-                    if (noOccupiedSeatsAdjacent(chairMap, i, j, maxX, maxY)) {
+                    // instead of using number Adjacent occupied here we could use a similar 
+                    // function and break early, i.e. on an occupied. But I didn't need the speed.
+                    if (numberAdjacentOccupied(chairMap, i, j, maxX, maxY) == 0) {
                         char[] chars = newChairMap.get(j);
                         chars[i] = '#';
                         newChairMap.put(j, chars);
                     }
                 }
                 else if (isOccupied(c)) {
-                    if (fourOrMoreAdjacentOccupied(chairMap, i , j, maxX, maxY)) {
+                    if (numberAdjacentOccupied(chairMap, i , j, maxX, maxY) >= 4) {
                         char[] chars = newChairMap.get(j);
                         chars[i] = 'L';
                         newChairMap.put(j, chars);
@@ -53,44 +67,40 @@ public class DayEleven {
         return newChairMap;
     }
 
-    // if seat is empty and there are no adjacent seats occupied; return true
-    // if seat is empty and at least 1 adjacent seat is occupied; return false;
-    private boolean noOccupiedSeatsAdjacent(Map<Integer, char[]> chairMap, int x, int y, int maxX, int maxY) {
-        // int seatsNotExisting = 0;
-        char c = chairMap.get(y)[x];
-        if (!isEmpty(c)) {
-            return false;
-        }
-        for (int i = x-1; i <= x+1; i++) {
-            for (int j = y-1; j <= y+1; j++) {
-                if (i < 0 || j < 0 || i >= maxX || j >= maxY) {
-                    // seatsNotExisting++;
-                    continue;
+    // @TODO yuck probs shouldn't just copy this. Should pull out common code.
+    private Map<Integer, char[]> occupySeatsPart2(final Map<Integer, char[]> chairMap) {
+        Map<Integer, char[]> newChairMap = deepClone(chairMap);
+        int maxX = chairMap.get(0).length;
+        int maxY = chairMap.keySet().size();
+        for (int i = 0; i < maxX ; i++) {
+            for (int j = 0; j < maxY; j++) {
+                char c = chairMap.get(j)[i];
+                if (isEmpty(c)) {
+                    if (numberAdjacentOccupiedPart2(chairMap, i, j, maxX, maxY) == 0) {
+                        char[] chars = newChairMap.get(j);
+                        chars[i] = '#';
+                        newChairMap.put(j, chars);
+                    }
                 }
-                if (i == x && j == y) {
-                    continue;
-                }
-                if (isOccupied(chairMap.get(j)[i])) {
-                    return false;
+                else if (isOccupied(c)) {
+                    if (numberAdjacentOccupiedPart2(chairMap, i , j, maxX, maxY) >= 5) {
+                        char[] chars = newChairMap.get(j);
+                        chars[i] = 'L';
+                        newChairMap.put(j, chars);
+                    }
                 }
             }
         }
-        return true;
+        
+        return newChairMap;
     }
 
-    // return true if four or more seats adjacent are occupied.
-    private boolean fourOrMoreAdjacentOccupied(Map<Integer, char[]> chairMap, int x, int y, int maxX, int maxY) {
-        char c = chairMap.get(y)[x];
-        if (!isOccupied(c)) {
-            return false;
-        }
+    // return number of directly adjacent seats that are occupied.
+    private int numberAdjacentOccupied(Map<Integer, char[]> chairMap, int x, int y, int maxX, int maxY) {
         int adjacentOccupied = 0;
         for (int i = x-1; i <= x+1; i++) {
             for (int j = y-1; j <= y+1; j++) {
-                if (i < 0 || j < 0 || i >= maxX || j >= maxY) {
-                    continue;
-                }
-                if (i == x && j == y) {
+                if (i < 0 || j < 0 || i >= maxX || j >= maxY || (i == x && j == y)) {
                     continue;
                 }
                 if (isOccupied(chairMap.get(j)[i])) {
@@ -98,7 +108,50 @@ public class DayEleven {
                 }
             }
         }
-        return adjacentOccupied >= 4;
+        return adjacentOccupied;
+    }
+
+    // return the number of adjacent seats that are occupied. This includes any seat in a particular direction.
+    private int numberAdjacentOccupiedPart2(Map<Integer, char[]> chairMap, int x, int y, int maxX, int maxY) {
+        int adjacentOccupied = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) { continue; }
+                if (isDirectionOccupied(chairMap, x, y, maxX, maxY, i, j)) {
+                    adjacentOccupied++;
+                }
+            }
+        }
+        return adjacentOccupied;
+    }
+
+    // part 2 check all seats in a particular direction.
+    // return true if found occcupied
+    // return false if empty
+    // continue in direction if neither.
+    private boolean isDirectionOccupied(
+        final Map<Integer, char[]> chairMap, 
+        final int startX, 
+        final int startY, 
+        final int maxX, 
+        final int maxY, 
+        final int changeX, 
+        final int changeY
+    ) {
+        int i = startX + changeX;
+        int j = startY + changeY;
+        while (i >= 0 && i < maxX && j>= 0 && j < maxY) {
+            char seat = chairMap.get(j)[i];
+            if (isOccupied(seat)) {
+                return true;
+            }
+            if (isEmpty(seat)) {
+                return false;
+            }
+            i = i + changeX;
+            j = j + changeY;
+        }
+        return false;
     }
 
     private boolean isOccupied(char seat) {
