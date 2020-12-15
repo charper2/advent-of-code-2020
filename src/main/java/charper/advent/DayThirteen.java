3949fee2a014cf8ad9cdcc20ec7d118bcb585409
EC2 Default User <ec2-user@ip-172-31-20-222.us-east-2.compute.internal>
2020-12-15 07:01:18 +0000

    Day 13 Part 2

--- src/main/java/charper/advent/DayThirteen.java
+++ src/main/java/charper/advent/DayThirteen.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class DayThirteen {
@@ -32,8 +31,58 @@ public class DayThirteen {
     }
 
     private Long run2(String filePath) {
-        Map<Long, Long> times = getTimes();
+        Timetable timetable = getTimetable(filePath);
+        List<Long> times = timetable.getBuses();
+        List<Integer> remainders = timetable.getRemainders();
+        // remainders.remove(remainders.size() - 1);
+        // remainders.add(0);
+        System.out.println(times);
+        System.out.println(remainders);
         
+        // Chinese remainder theorem
+        Long product = 1L;
+        for (Long time : times) {
+             product = product * time;
+        }
+
+        List<Long> partialProducts = new ArrayList<>();
+        for (Long time : times) {
+            partialProducts.add(product / time);
+        }
+        
+        Long sum = 0L;
+        for (int i = 0; i < times.size(); i++) {
+            sum += partialProducts.get(i) * computeInverse(partialProducts.get(i), times.get(i)) * remainders.get(i);
+        }
+        return sum % product;
+    }
+    
+    private static long computeInverse(long a, long b) { 
+        long m = b, t, q; 
+        long x = 0, y = 1; 
+      
+        if (b == 1) 
+            return 0; 
+      
+        // Apply extended Euclid Algorithm 
+        while (a > 1) 
+        { 
+            // q is quotient 
+            q = a / b; 
+            t = b; 
+      
+            // now proceed same as Euclid's algorithm 
+            b = a % b;a = t; 
+            t = x; 
+            x = y - q * x; 
+            y = t; 
+        } 
+      
+        // Make x1 positive 
+        if (y < 0) 
+         y += m; 
+      
+        return y; 
     }
 
     private Timetable getTimetable(String filePath) {
@@ -46,7 +95,10 @@ public class DayThirteen {
             Timetable timetable = new Timetable(timestamp);
             for (int i = 0; i < timesArray.length; i++) {
                 if (!timesArray[i].equals("x")) {
-                    timetable.addBus(Long.parseLong(timesArray[i]));
+                    Long bus = Long.parseLong(timesArray[i]);
+                    Integer busInt = Integer.parseInt(timesArray[i]);
+                    timetable.addBus(bus);
+                    timetable.addRemainder(busInt-i);
                 }
             }
             scanner.close();
@@ -60,6 +112,7 @@ public class DayThirteen {
     private static class Timetable {
         private final Long timestamp;
         private List<Long> buses = new ArrayList<>();
+        private List<Integer> remainders = new ArrayList<>();
 
         public Timetable(Long timestamp) {
             this.timestamp = timestamp;
@@ -75,6 +128,14 @@ public class DayThirteen {
 
         public void addBus(Long bus) {
             buses.add(bus);
+        }
+        
+        public List<Integer> getRemainders() {
+            return this.remainders;
+        }
+
+        public void addRemainder(Integer remainder) {
+            remainders.add(remainder);    
         }
     }
 }
